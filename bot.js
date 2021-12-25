a32418fd08fc84c208ec64d34c0de8da3a00322b
Nguyễn Đình Khôi <49549370+ngdkhoi@users.noreply.github.com>
2021-12-25 20:24:19 +0700

    first commit

---
+++ bot.js
@@ -0,0 +1,86 @@
+const TelegramBot = require("node-telegram-bot-api");
+require('dotenv').config();
+
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.BOT_TOKEN;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  
+    const chatId = msg.chat.id;
+    bot.sendMessage(
+      chatId, ` Hello ${msg.chat.username}, this is our information
+    /twitter - twitter link
+    /facebook - facebook link
+    /telegram - our telegram group
+    /github - our github
+    /about - about us
+    /website - our website
+    /whitepaper - our whitepaper
+    /token - token address`
+    );
+  });
+
+  bot.onText(/\/twitter/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(
+      chatId, `${process.env.TWITTER_LINK}`
+    );
+  });
+
+  bot.onText(/\/facebook/, (msg) => {
+    const chatId = msg.chat.id;
+  
+    bot.sendMessage(
+      chatId, `${process.env.FACEBOOK_LINK}`
+    );
+  });
+
+  bot.onText(/\/telegram/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(
+      chatId, `${process.env.TELEGRAM_LINK}`
+    );
+  });
+
+  bot.onText(/\/github/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(
+      chatId, `${process.env.GITHUB_LINK}`
+    );
+  });
+
+  bot.onText(/\/about/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(
+      chatId, `${process.env.ABOUT_LINK}`
+    );
+  });
+
+  bot.onText(/\/website/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(
+      chatId, `${process.env.WEBSITE_LINK}`
+    );
+  });
+
+  bot.onText(/\/whitepaper/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(
+      chatId, `${process.env.WHITEPAPER}`);
+  });
+
+  bot.onText(/\/token/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(
+      chatId, `${process.env.TOKEN_ADDRESS}`);
+  });
+
+  bot.onText('message', (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(
+      chatId, `Hello ${msg.chat.username}, you can []`);
+  })
